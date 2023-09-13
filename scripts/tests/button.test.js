44d439f50f487a2caa21451d24e3fb964cd34090
2ndborn <>
2023-09-13 12:02:06 +0000

    Finalised testing

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,12 +5,19 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 })
 
 describe("DOM tests", () => {
-    test("Expects p content to change", () => {
+    test("expects p content to change", () => {
         buttonClick();
         expect(getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementById("h1").length).toBe(1)
     });
 });
